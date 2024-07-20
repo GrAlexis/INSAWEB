f0b0c9ad11778de8df2cecbb99dcdcb3bac19bb2
unknown <tristanverdet@gmail.com>
2024-07-20 20:19:57 +0200

    Changed the url for login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Login = () => {
   const handleLogin = () => {
-    window.location.href = 'http://localhost:3000/login';
+    window.location.href = 'http://localhost:5000/api/connexion/';
   };
 
   return (
