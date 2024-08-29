0480274491b91ac4f012d5a32bb832774c1ed50d
unknown <tristanverdet@gmail.com>
2024-08-29 19:03:24 +0200

    Utilisation du token et des cookies pour verifier qu'on est bien authent

--- client/src/utils/ProtectedRoute.js
+++ client/src/utils/ProtectedRoute.js
@@ -2,14 +2,55 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useUser } from '../hooks/commonHooks/UserContext';
 
-const ProtectedRoute = ({ children }) => {
-  const { user } = useUser();
+const isAuthenticated = async () => {
+  const token = sessionStorage.getItem('token');
+  
+  if (token) {
+    try {
+      const response = await fetch('http://localhost:5000/api/user/decode', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token }),
+      });
 
-  if (!user || !user.isAdmin) {
-    return <Navigate to="/login" />;
+      const data = await response.json();
+      
+      if (data.username) {
+        sessionStorage.setItem('username', data.username);
+        sessionStorage.setItem('isAdmin', data.isAdmin); // Stockez isAdmin dans le sessionStorage
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.error('Erreur lors de la vérification du token:', error);
+      return false;
+    }
   }
 
-  return children;
+  return sessionStorage.getItem('isAuthenticated') === 'true';
+};
+
+const ProtectedRoute = ({ children, adminOnly=false }) => {
+  const { user } = useUser();
+
+  if (!isAuthenticated()) {
+    return <Navigate to="/login" />;
+  }
+  else{
+    if (adminOnly){
+      const { user } = useUser();
+      if (!user || !user.isAdmin){
+        alert("Page réservée aux administrateur\nConnectez-vous avec un compte admin.")
+        return <Navigate to="/login" />
+      }
+    }
+    return children;
+  }
+
+  
 };
 
 export default ProtectedRoute;
