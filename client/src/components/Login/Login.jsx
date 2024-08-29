32aaab57688d50f38f82b0515fc36e0a41e4aac0
unknown <tristanverdet@gmail.com>
2024-08-29 18:56:19 +0200

    coquille url

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('MY_URL', {
+      const response = await fetch('http://localhost:5000/api/user/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
