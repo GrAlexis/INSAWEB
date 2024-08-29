2647e30773300140cef87a9e15e2bb805bfba29a
unknown <tristanverdet@gmail.com>
2024-08-29 18:52:53 +0200

    ajout de email

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     lastName: {type:String, required:true},
+    email: {type:String, required:true},
     hashedPassword: {type:String, requiered:true},
     classYear: { type: String, required: true },
     rank: { type: Number},
