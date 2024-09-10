7808986264efaf6cdc1f8fc34a63d7b534034924
tom goarin <tom.goarin@gmail.com>
2024-09-10 20:49:40 +0200

    added fetch all users route

--- server/server.js
+++ server/server.js
@@ -466,6 +466,16 @@ app.put('/challenges/:id', async (req, res) => {
       res.status(500).send('Error updating challenge: ' + error.message);
     }
   });
+
+//route to fetch all users
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
   
 
 
