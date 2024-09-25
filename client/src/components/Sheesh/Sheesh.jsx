0781b5ae38180de1dc4dd6e95eb6d95d745dae8b
tom goarin <tom.goarin@gmail.com>
2024-09-25 19:55:05 +0200

    fixed looping

--- client/src/components/Sheesh/Sheesh.jsx
+++ client/src/components/Sheesh/Sheesh.jsx
@@ -65,7 +65,7 @@ const Sheesh = ({ showNavBar }) => {
     fetchEvents();
     fetchChallenges();
     calculatePinnedChallenges();
-  }, [user, challenges]);
+  }, [user]);
 
   useEffect(() => {
     if (challengeId && challengeRefs.current[challengeId]) {
