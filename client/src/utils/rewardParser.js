2de0bc9749feea2e47487bea33714391364ed647
tom goarin <tom.goarin@gmail.com>
2024-10-09 11:26:49 +0200

    fixed majuscule dans les sheesh

--- client/src/utils/rewardParser.js
+++ client/src/utils/rewardParser.js
@@ -1,7 +1,7 @@
 import config from '../config';
 // utils/rewardParser.js
 export const parseReward = (rewardString) => {
-    const regex = /(\d+)\s*sh/;
+    const regex = /(\d+)\s*sh/i;
     const match = regex.exec(rewardString);
     if (match) {
         return parseInt(match[1], 10);
