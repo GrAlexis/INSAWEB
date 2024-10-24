7ede4b8a67575e10fc908d6b1ac4be4cc45b1f31
tom goarin <tom.goarin@gmail.com>
2024-10-24 08:26:17 +0200

    badge carrousel réparé

--- client/src/components/InfoBar/BadgeCarrousel.jsx
+++ client/src/components/InfoBar/BadgeCarrousel.jsx
@@ -4,15 +4,16 @@ import config from '../../config';
 import axios from 'axios';
 import { useUniverse } from '../../hooks/commonHooks/UniverseContext'; // Importer le hook UniverseContext
 import { useUser } from '../../hooks/commonHooks/UserContext';
+import defaultBadge from '../../assets/buttons/chokbar.png';
 
 
 const BadgeCarouselComponent = ({ universes }) => {
   const { user } = useUser();
   const [joinedUniverses, setJoinedUniverses] = useState([]); // State for storing the badges dynamically
-  const [selectedUniverseLocal, setSelectedUniverseLocal] = useState(joinedUniverses[0]);
   const [showCarousel, setShowCarousel] = useState(false);
   const [closing, setClosing] = useState(false);
   const { selectedUniverse, saveUniverse } = useUniverse();
+  const [loading, setLoading] = useState(false);
     
   // Fetch the joined universes of the user and build the badges array
   useEffect(() => {
@@ -29,10 +30,6 @@ const BadgeCarouselComponent = ({ universes }) => {
         
         setJoinedUniverses(fetchedJoinedUniverses);
 
-        // Set the first badge as the selected badge by default
-        if (fetchedJoinedUniverses.length > 0) {
-          setSelectedUniverseLocal(fetchedJoinedUniverses[0]);
-        }
       } catch (error) {
         console.error('Error fetching joined universes:', error);
       }
@@ -43,8 +40,7 @@ const BadgeCarouselComponent = ({ universes }) => {
     }
   }, [user]); // Run the effect when the user is loaded
 
-  const handleBadgeClick = (universe) => {
-    setSelectedUniverseLocal(universe);
+  const handleUniverseClick = (universe) => {
     setClosing(true);
 
     // Trouver l'univers correspondant au badge et changer d'univers
@@ -53,7 +49,6 @@ const BadgeCarouselComponent = ({ universes }) => {
       saveUniverse(selectedUniverse);
     } else {
       console.error('Univers non trouvé pour ce badge.');
-
     }
 
     // Gérer la fermeture avec un délai pour permettre l'animation de fermeture
@@ -81,20 +76,20 @@ const BadgeCarouselComponent = ({ universes }) => {
       {/* Affiche un badge par défaut si aucun badge n'est sélectionné */}
       {loading ? (
         <div className="loading-spinner">Chargement...</div>
-      ) : !selectedBadge ? (
+      ) : !selectedUniverse ? (
         <div className="default-badge" onClick={() => setShowCarousel(true)}>
           <img src={defaultBadge} alt="Badge par défaut" className="large-badge" />
           <p>Rejoindre un univers</p>
         </div>
       ) : (
         <div className="selected-badge" onClick={handleMainBadgeClick}>
-          <img src={selectedBadge.src} alt={selectedBadge.alt} className="large-badge" />
+          <img src={selectedUniverse.logo} alt={selectedUniverse.name} className="large-badge" />
         </div>
       )}
 
-      <div className="selected-badge" onClick={() => setShowCarousel(!showCarousel)}>
+      {/* <div className="selected-badge" onClick={() => setShowCarousel(!showCarousel)}>
         <img src={selectedUniverse.logo} alt={`Badge ${selectedUniverse.name}`} className="large-badge" />
-      </div>
+      </div> */}
 
       {showCarousel && (
         <div className={`carousel ${closing ? 'closing' : ''}`}>
@@ -102,8 +97,8 @@ const BadgeCarouselComponent = ({ universes }) => {
             {joinedUniverses.map((joinedUniverse) => (
               <div
                 key={joinedUniverse._id}
-                className={`badge-item ${selectedUniverseLocal._id === joinedUniverse._id ? 'active' : ''}`}
-                onClick={() => handleBadgeClick(joinedUniverse)}
+                className={`badge-item ${selectedUniverse._id === joinedUniverse._id ? 'active' : ''}`}
+                onClick={() => handleUniverseClick(joinedUniverse)}
               >
                 <img src={joinedUniverse.logo} alt={joinedUniverse.name} />
               </div>
